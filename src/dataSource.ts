1fda5f70fd9821b49c1a12fc9c4886eff19c2a9a
ivaylo-kostov-it <ivaylo.kostov.it@gmail.com>
2025-04-08 03:45:24 +0300

    use different db host for Docker network and local Node.js

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -5,7 +5,7 @@ import { Assignment } from './entities/Assignment';
 
 export const dataSource = new DataSource({
     type: 'postgres',
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.DATA_SOURCE_HOST || 'localhost',
     port: parseInt(process.env.POSTGRES_PORT || '5432'),
     username: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || 'blackdeep',
